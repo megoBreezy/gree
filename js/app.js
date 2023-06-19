e3278bfabdf52461bd556687a80fa9a325801d8e
rehush <a.rehush@fairtech.group>
2023-06-19 22:50:15 +0300

    19.06.2023

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
+window.addEventListener('beforeunload', function() {
+	document.body.style.opacity = 0;
+	window.scrollTo(0, 0);
+
+});
+
 window.addEventListener("DOMContentLoaded", () => {
   
 	let locoScroll = null;
+  var scrollContainer = document.querySelector('[data-scroll-container]');
 	let updateScroll = function updateScroll(scrollContainer) {
 		new ResizeObserver(function () {
 			return locoScroll.update();
@@ -8,34 +15,41 @@ window.addEventListener("DOMContentLoaded", () => {
 	};
   gsap.registerPlugin(ScrollTrigger);
   gsap.registerPlugin(Draggable);
-	const scrollContainer = document.querySelector(".scroll-container")
-	locoScroll = new LocomotiveScroll({
-		el: scrollContainer,
-		smooth: true,
-		tablet: {
-			breakpoint: 576,
-			smooth: true
-		},
-		smartphone: {
-			breakpoint: 0,
-			smooth: false
-		}
-	});
-	locoScroll.on("scroll", ScrollTrigger.update);
 
-	ScrollTrigger.scrollerProxy(scrollContainer, {
-		scrollTop(value) {
-			return arguments.length 
-			? locoScroll.scrollTo(value, 0, 0) 
-			: locoScroll.scroll.instance.scroll.y;
-		},
-		getBoundingClientRect() {
-			return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
-		},
-		pinType: scrollContainer.style.transform ? "transform" : "fixed"
-	});
-	ScrollTrigger.defaults({ scroller: scrollContainer });
-	updateScroll(scrollContainer);
+  function initScroll() {
+    var scrollContainer = document.querySelector(".scroll-container")
+    locoScroll = new LocomotiveScroll({
+      el: scrollContainer,
+      smooth: true,
+      getDirection: true,
+      tablet: {
+        breakpoint: 576,
+        smooth: true
+      },
+      smartphone: {
+        breakpoint: 0,
+        smooth: false
+      }
+    });
+    locoScroll.on("scroll", ScrollTrigger.update);
+  
+    ScrollTrigger.scrollerProxy(scrollContainer, {
+      scrollTop(value) {
+        return arguments.length 
+        ? locoScroll.scrollTo(value, 0, 0) 
+        : locoScroll.scroll.instance.scroll.y;
+      },
+      getBoundingClientRect() {
+        return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+      },
+      pinType: scrollContainer.style.transform ? "transform" : "fixed"
+    });
+    ScrollTrigger.defaults({ scroller: scrollContainer });
+    updateScroll(scrollContainer);
+
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+    ScrollTrigger.refresh();
+  }
 
   // accordion 
   const accordionHeaders = document.querySelectorAll('.accordion__header');
@@ -353,11 +367,12 @@ window.addEventListener("DOMContentLoaded", () => {
     });
   });
 
+
   function modalsClose() {
     if (document.querySelector('.modal--active .call-back')) {
       document.querySelector('.modal--active .call-back').classList.remove('call-back--active');
     }
-
+    
     document.querySelectorAll('.modal').forEach(modal => {
       modal.classList.remove('modal--active');
       document.body.classList.remove('modalOpen');
@@ -365,7 +380,10 @@ window.addEventListener("DOMContentLoaded", () => {
   }
 
   document.querySelectorAll('.modal__close').forEach(close => {
-    close.addEventListener('click', modalsClose);
+    close.addEventListener('click', function () {
+      locoScroll.start();
+      modalsClose();
+    });
   });
 
   document.querySelectorAll('.modal').forEach(modal => {
@@ -524,21 +542,25 @@ window.addEventListener("DOMContentLoaded", () => {
   }
 
 	//header
-	const header = document.querySelector('header');
-	locoScroll.on('scroll', function (e) {
-		scrollEvent(e);
-	});
-	let prevScrollpos = window.pageYOffset;
-	function scrollEvent(data) {
-		let currentScrollPos = data.scroll.y;
+  function initShowHideHeader() {
 
-		if (currentScrollPos > prevScrollpos) {
-			header.classList.add('header-offset');
-		} else {
-			header.classList.remove('header-offset');
-		}
-		prevScrollpos = currentScrollPos;
-	}
+    const header = document.querySelector('.header');
+  
+    const showHeaderAnim = gsap.from(header, { 
+      yPercent: -100,
+      paused: true,
+      duration: 0.3
+    }).progress(1);
+  
+    ScrollTrigger.create({
+      scroller: scrollContainer,
+      start: 'top top',
+      end: 99999,
+      onUpdate: (self) => {
+        self.direction === -1 ? showHeaderAnim.play() : showHeaderAnim.reverse();
+      }
+    });
+  }
 
   //tabs
   if (document.querySelector('.tab-wrap')) {
@@ -572,240 +594,242 @@ window.addEventListener("DOMContentLoaded", () => {
     });
   }
 
+  //partners
+  function partnersAnimate() {
+    const advantagesItems = document.querySelectorAll('.partners__item');
+  
+    advantagesItems.forEach((element, index) => {
+      gsap.from(element, {
+        scrollTrigger: {
+          trigger: element,
+          scroll: scrollContainer,
+          start: "top 90%",
+        },
+        x: 100,
+        opacity: 0,
+        duration: .6,
+        ease: "Expo.easeOut",
+      });
+    });
+  }
+
   //block animation
-    function partnersAnimate() {
-      const advantagesItems = document.querySelectorAll('.partners__item');
+  setTimeout(() => {
+    initScroll();
+  }, 10);
+  setTimeout(() => {
+
+    initShowHideHeader();
+
+    if (document.querySelector(".services-intro")) {
+      const container = document.querySelector(".services-intro");
+      const scrollbar = document.querySelector('.services-intro-scrollbar');
+      const handler = document.querySelector('.services-intro-scrollbar__handler');
+      const scrollbarLength = scrollbar.offsetWidth - handler.offsetWidth;
+  
+      let teamWrap = document.querySelector(".services-intro");
+      let teamWrapWidth = teamWrap.scrollWidth;
+      let backPadding = 300;
+      let teamWrapWidthOffset = teamWrapWidth - window.innerWidth;
+  
+      if (window.innerWidth < 1200) backPadding = 100
+  
+      let trigger = gsap.to(container, {
+        scrollTrigger: {
+          scrub: true,
+          trigger: ".services-intro-section",
+          pin: ".services-intro-section",
+          scroll: scrollContainer,
+          start: "top top",
+          end: () => "+=" + (teamWrapWidth + backPadding) / 1.5,
+          onUpdate: self => updateHandler(self.progress)
+        },
+        x: - (teamWrapWidthOffset + backPadding),
+        ease: "none"
+      });
+  
+      let draggable = Draggable.create(handler, {
+        type: "x",
+        bounds: ".services-intro-scrollbar",
+        onDrag: function () {
+          const section = trigger.scrollTrigger;
+          const range = gsap.utils.mapRange(this.minX, this.maxX, section.start, section.end, this.x);
+          section.scroll(range);
+        }
+      });
+  
+      function updateHandler(progress) {
+        const range = gsap.utils.mapRange(0, 1, 0, scrollbarLength, progress);
+        gsap.set(handler, { x: range });
+      }
+    }
+    if (document.querySelector(".history-wrapper") && window.innerWidth > 767) {
+      gsap.to('.history-year', {
+        scrollTrigger: {
+          scroll: scrollContainer,
+          scrub: true,
+          trigger: ".history-wrapper",
+          pin: ".history-year",
+          start: "top center",
+          end: "bottom 80%",
+        },
+        ease: "none"
+      });
+    }
+    if (document.querySelector(".news__left") && window.innerWidth > 767) {
+      gsap.to('.news__left', {
+        scrollTrigger: {
+          scroll: scrollContainer,
+          scrub: true,
+          trigger: ".news",
+          pin: ".news__left",
+          start: "top 15%",
+          end: "bottom 80%",
+        },
+        ease: "none"
+      });
+    }
+
+    if(document.querySelector('.fade-left')) {
+      const itemsLeft = document.querySelectorAll('.fade-left');
+      itemsLeft.forEach(element => {
+        let delayValue = element.getAttribute("data-delay") || 0;
+        
+        gsap.from(element, {
+          scrollTrigger: {
+            trigger: element,
+            scroll: scrollContainer,
+            start: "top 80%",
+          },
+          x: 150,
+          opacity: 0,
+          duration: .6,
+          ease: "Power0.easeIn",
+          delay: delayValue
+        });
+      });
+    }
+
+    if(document.querySelector('.fade-right')) {
+      const itemsLeft = document.querySelectorAll('.fade-right');
+      itemsLeft.forEach(element => {
+        let delayValue = element.getAttribute("data-delay") || 0;
+        
+        gsap.from(element, {
+          scrollTrigger: {
+            trigger: element,
+            scroll: scrollContainer,
+            start: "top 80%",
+          },
+          x: -150,
+          opacity: 0,
+          duration: .6,
+          ease: "Power0.easeIn",
+          delay: delayValue
+        });
+      });
+    }
+    if (document.querySelector('.advantages-grid')) {
+      const advantagesItems = document.querySelectorAll('.advantages-item');
     
       advantagesItems.forEach((element, index) => {
         gsap.from(element, {
           scrollTrigger: {
             trigger: element,
             scroll: scrollContainer,
-            start: "top 90%",
+            start: "top 95%",
+            end: "bottom 60%",
+            scrub: true,
           },
-          x: 100,
+          x: index % 2 === 0 ? -150 : 150,
           opacity: 0,
-          duration: .6,
-          ease: "Expo.easeOut",
         });
       });
     }
-    setTimeout(() => {
-      if (document.querySelector(".services-intro")) {
-        const container = document.querySelector(".services-intro");
-        const scrollbar = document.querySelector('.services-intro-scrollbar');
-        const handler = document.querySelector('.services-intro-scrollbar__handler');
-        const scrollbarLength = scrollbar.offsetWidth - handler.offsetWidth;
+    if (document.querySelector('.about-media__img')) {
+      const advantagesItems = document.querySelectorAll('.about-media__img img');
     
-        let teamWrap = document.querySelector(".services-intro");
-        let teamWrapWidth = teamWrap.scrollWidth;
-        let backPadding = 300;
-        let teamWrapWidthOffset = teamWrapWidth - window.innerWidth;
-    
-        if (window.innerWidth < 1200) backPadding = 100
-    
-        let trigger = gsap.to(container, {
+      advantagesItems.forEach((element) => {
+        gsap.to(element, {
           scrollTrigger: {
-            scrub: true,
-            trigger: ".services-intro-section",
-            pin: ".services-intro-section",
+            trigger: element,
             scroll: scrollContainer,
-            start: "top top",
-            end: () => "+=" + (teamWrapWidth + backPadding),
-            onUpdate: self => updateHandler(self.progress)
+            start: "top 95%",
+            end: "bottom 80%",
+            scrub: 0.5,
           },
-          x: - (teamWrapWidthOffset + backPadding),
-          ease: "none"
+          scale: 1,
+
         });
+      });
+    }
+    if (document.querySelector('.partners__item')) {
+      partnersAnimate();
+    }
+    if (document.querySelector('.leadership__title')) {
+      const advantagesItems = document.querySelectorAll('.leadership__title');
     
-        let draggable = Draggable.create(handler, {
-          type: "x",
-          bounds: ".services-intro-scrollbar",
-          onDrag: function () {
-            const section = trigger.scrollTrigger;
-            const range = gsap.utils.mapRange(this.minX, this.maxX, section.start, section.end, this.x);
-            section.scroll(range);
+      advantagesItems.forEach((element) => {
+
+        gsap.from(element, {
+          scrollTrigger: {
+            trigger: element,
+            scroll: scrollContainer,
+            start: "top 80%",
+            toggleClass: 'fade-in',
           }
         });
+      });
+    }
+    if (document.querySelector('.news-list__item')) {
+      const advantagesItems = document.querySelectorAll('.news-list__item');
     
-        function updateHandler(progress) {
-          const range = gsap.utils.mapRange(0, 1, 0, scrollbarLength, progress);
-          gsap.set(handler, { x: range });
-        }
-      }
-      if (document.querySelector(".history-wrapper") && window.innerWidth > 767) {
-        gsap.to('.history-year', {
+      advantagesItems.forEach((element) => {
+
+        gsap.from(element, {
           scrollTrigger: {
+            trigger: element,
             scroll: scrollContainer,
-            scrub: true,
-            trigger: ".history-wrapper",
-            pin: ".history-year",
-            start: "top center",
-            end: "bottom 80%",
-          },
-          ease: "none"
+            start: "top 80%",
+            toggleClass: 'animated',
+          }
         });
-      }
-      if (document.querySelector(".news__left") && window.innerWidth > 767) {
-        gsap.to('.news__left', {
-          scrollTrigger: {
-            scroll: scrollContainer,
-            scrub: true,
-            trigger: ".news",
-            pin: ".news__left",
-            start: "top top",
-            end: "bottom 70%",
-          },
-          ease: "none"
-        });
-      }
-
-      if(document.querySelector('.fade-left')) {
-        const itemsLeft = document.querySelectorAll('.fade-left');
-        itemsLeft.forEach(element => {
-          let delayValue = element.getAttribute("data-delay") || 0;
-          
-          gsap.from(element, {
-            scrollTrigger: {
-              trigger: element,
-              scroll: scrollContainer,
-              start: "top 80%",
-            },
-            x: 150,
-            opacity: 0,
-            duration: .6,
-            ease: "Power0.easeIn",
-            delay: delayValue
-          });
-        });
-      }
-
-      if(document.querySelector('.fade-right')) {
-        const itemsLeft = document.querySelectorAll('.fade-right');
-        itemsLeft.forEach(element => {
-          let delayValue = element.getAttribute("data-delay") || 0;
-          
-          gsap.from(element, {
-            scrollTrigger: {
-              trigger: element,
-              scroll: scrollContainer,
-              start: "top 80%",
-            },
-            x: -150,
-            opacity: 0,
-            duration: .6,
-            ease: "Power0.easeIn",
-            delay: delayValue
-          });
-        });
-      }
-      if (document.querySelector('.advantages-grid')) {
-        const advantagesItems = document.querySelectorAll('.advantages-item');
-      
-        advantagesItems.forEach((element, index) => {
-          gsap.from(element, {
-            scrollTrigger: {
-              trigger: element,
-              scroll: scrollContainer,
-              start: "top 95%",
-              end: "bottom 60%",
-              scrub: true,
-            },
-            x: index % 2 === 0 ? -150 : 150,
-            opacity: 0,
-          });
-        });
-      }
-      if (document.querySelector('.about-media__img')) {
-        const advantagesItems = document.querySelectorAll('.about-media__img img');
-      
-        advantagesItems.forEach((element) => {
-          gsap.to(element, {
-            scrollTrigger: {
-              trigger: element,
-              scroll: scrollContainer,
-              start: "top 95%",
-              end: "bottom 80%",
-              scrub: 0.5,
-            },
-            scale: 1,
-
-          });
-        });
-      }
-      if (document.querySelector('.partners__item')) {
-        partnersAnimate();
-      }
-      if (document.querySelector('.leadership__title')) {
-        const advantagesItems = document.querySelectorAll('.leadership__title');
-      
-        advantagesItems.forEach((element) => {
-
-          gsap.from(element, {
-            scrollTrigger: {
-              trigger: element,
-              scroll: scrollContainer,
-              start: "top 80%",
-              toggleClass: 'fade-in',
-            }
-          });
-        });
-      }
-      if (document.querySelector('.news-list__item')) {
-        const advantagesItems = document.querySelectorAll('.news-list__item');
-      
-        advantagesItems.forEach((element) => {
-
-          gsap.from(element, {
-            scrollTrigger: {
-              trigger: element,
-              scroll: scrollContainer,
-              start: "top 80%",
-              toggleClass: 'animated',
-            }
-          });
-        });
-      }
-      if (document.querySelector('.welcome__logo')) {
-        const welcomeLogo = document.querySelectorAll('.welcome__logo');
-      
-        welcomeLogo.forEach((element) => {
-
-          gsap.to(element, {
-            scrollTrigger: {
-              trigger: element,
-              scroll: scrollContainer,
-              start: "top 80%",
-            },
-            delay: 0.3,
-            opacity: 1,
-            scale: 1,
-            ease: Power0.easeIn,
-          });
-        });
-      }
+      });
+    }
+    if (document.querySelector('.welcome__logo')) {
+      const welcomeLogo = document.querySelectorAll('.welcome__logo');
     
-      if(document.querySelector('.map__img')) {
-        let mapTl = gsap.timeline({
+      welcomeLogo.forEach((element) => {
+
+        gsap.to(element, {
           scrollTrigger: {
-            trigger: '.map__img',
+            trigger: element,
             scroll: scrollContainer,
             start: "top 80%",
           },
+          delay: 0.3,
+          opacity: 1,
+          scale: 1,
+          ease: Power0.easeIn,
         });
-        mapTl
-          .from('.map__img', {opacity: 0, scale: 0.7, duration: 0.7})
-          .from('.map-dot-1', {opacity: 0, scale: 0, duration: 0.3})
-          .from('.map-dot-2', {opacity: 0, scale: 0, duration: 0.3})
-          .from('.map-dot-3', {opacity: 0, scale: 0, duration: 0.3}, "-=0.8")
-          .from('.map-dot-4', {opacity: 0, scale: 0, duration: 0.3}, "-=0.6")
-          .from('.map-dot-5', {opacity: 0, scale: 0, duration: 0.3})
-      }
-    }, 300);
-
-	ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-	ScrollTrigger.refresh();
-  if(window.innerWidth < 1024) {
-    ScrollTrigger.normalizeScroll(true);
-  }
+      });
+    }
+  
+    if(document.querySelector('.map__img')) {
+      let mapTl = gsap.timeline({
+        scrollTrigger: {
+          trigger: '.map__img',
+          scroll: scrollContainer,
+          start: "top 80%",
+        },
+      });
+      mapTl
+        .from('.map__img', {opacity: 0, x: 150, duration: 0.7})
+        .from('.map-dot-1', {opacity: 0, scale: 0, duration: 0.3})
+        .from('.map-dot-2', {opacity: 0, scale: 0, duration: 0.3})
+        .from('.map-dot-3', {opacity: 0, scale: 0, duration: 0.3}, "-=0.8")
+        .from('.map-dot-4', {opacity: 0, scale: 0, duration: 0.3}, "-=0.6")
+        .from('.map-dot-5', {opacity: 0, scale: 0, duration: 0.3})
+    }
+  }, 100);
 });
